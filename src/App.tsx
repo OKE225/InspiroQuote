923fe0d40434a1c777537c2d461723ca2e170ca6
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-04-03 10:18:47 +0200

    Create navigation components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import "./App.css";
+import Box from "@mui/material/Box";
+import PageHeader from "./components/PageHeader";
+import AuthorInfo from "./components/AuthorInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bgcolor={"#000815"} width="100%" height="100vh">
+      <nav>
+        <PageHeader>InspiroQuote</PageHeader>
+        <AuthorInfo />
+      </nav>
+    </Box>
   );
 }
 
---
+++ src/components/AuthorInfo.tsx
@@ -0,0 +1,33 @@
+import { Box, Link, Typography } from "@mui/material";
+import React from "react";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MessageIcon from "@mui/icons-material/Message";
+
+const AuthorInfo = () => {
+  return (
+    <Box position={"absolute"} right={15} top={10}>
+      <Box display={"flex"} color={"#0071E1"} justifyContent={"center"}>
+        <Link
+          href="https://www.google.pl/?hl=pl"
+          width={24}
+          height={24}
+          mr={0.5}>
+          <GitHubIcon />
+        </Link>
+        <Link
+          href="https://www.google.pl/?hl=pl"
+          width={24}
+          height={24}
+          ml={0.5}>
+          <MessageIcon />
+        </Link>
+      </Box>
+      <Typography variant="h6" color="#023E7D">
+        by oke
+      </Typography>
+    </Box>
+  );
+};
+
+export default AuthorInfo;
