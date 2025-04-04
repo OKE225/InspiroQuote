9b29fc6ee263ce8640857f7ed9617326e45d44c5
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-04-04 21:34:46 +0200

    Updates

---
+++ static/js/952.95506bc1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinspiro_quote=self.webpackChunkinspiro_quote||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.95506bc1.chunk.js.map
